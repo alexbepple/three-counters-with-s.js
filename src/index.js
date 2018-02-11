ed18894c102786b1ec18990394a5098ee8a573ad
Alex Bepple <alex@bepple.de>
2018-02-11 18:29:24 +0100

    % add React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+import * as React from 'react'
+import {render} from 'react-dom'
+
+const App = () => <h2>Hello, World!</h2>
+
+render(
+  <App />,
+  document.getElementById('root')
+)
