af27e3ec45e7d696b48169eab5957cc3c6f5186a
Alex Bepple <alex@bepple.de>
2018-02-12 06:19:42 +0100

    . dry

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import * as React from 'react'
 import {render} from 'react-dom'
 import s from 's-js'
+import * as r from 'ramda'
 
 const signal = s.data(0)
+const signalLens = r.lens(s => s(), (val, s) => s(val))
+const over = r.over(signalLens)
 
 const App = () => <div>
   <h2>Count: {signal()}</h2>
-  <button onClick={() => signal(signal() + 1)}>+</button>
+  <button onClick={() => over(r.inc, signal)}>+</button>
 </div>
 
 s.root(() => s.on(
