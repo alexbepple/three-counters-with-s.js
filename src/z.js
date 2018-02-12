e91e830b4c7f38f7a37e5dae70e83172eccb41a5
Alex Bepple <alex@bepple.de>
2018-02-12 22:12:25 +0100

    % only pass explicit props

--- src/z.js
+++ src/z.js
@@ -6,10 +6,10 @@ const signalLens = r.lens(s => s(), (val, s) => s(val))
 export const over = r.over(signalLens)
 
 export const connect = mapOwnProps => re.compose(
-  re.withProps(mapOwnProps),
   re.lifecycle({
     componentDidMount() {
       s(() => this.setState(mapOwnProps(this.props)))
     }
-  })
+  }),
+  re.mapProps(mapOwnProps)
 )
