65e27b44df4b4edbab60cbbe7bbae795c8db483b
Alex Bepple <alex@bepple.de>
2018-02-12 06:02:30 +0100

    % rerender from user interaction

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import s from 's-js'
 
 const signal = s.data(0)
 
-const App = () => <h2>Count: {signal()}</h2>
+const App = () => <div>
+  <h2>Count: {signal()}</h2>
+  <button onClick={() => signal(signal() + 1)}>+</button>
+</div>
 
 s.root(() => s.on(
   [signal],
   () => render(<App />, document.getElementById('root'))
 ))
-
-setTimeout(() => signal(1), 1000)
