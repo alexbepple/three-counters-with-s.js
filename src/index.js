8e1bf1fcc81d3142f0179c56620a4c966ffcb57f
Alex Bepple <alex@bepple.de>
2018-02-12 21:27:46 +0100

    . hide state management from top level and from low-level presentation components

--- src/index.js
+++ src/index.js
@@ -3,16 +3,33 @@ import {render} from 'react-dom'
 import s from 's-js'
 import * as r from 'ramda'
 
-const signal = s.data(0)
 const signalLens = r.lens(s => s(), (val, s) => s(val))
 const over = r.over(signalLens)
 
-const App = () => <div>
-  <h2>Count: {signal()}</h2>
-  <button onClick={() => over(r.inc, signal)}>+</button>
+const CounterPresentation = (props) => <div>
+  <h2>Count: {props.value}</h2>
+  <button onClick={props.onIncrement}>+</button>
 </div>
 
-s.root(() => s.on(
-  [signal],
-  () => render(<App />, document.getElementById('root'))
-))
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: props.signal()}
+  }
+  componentDidMount() {
+    s(() => this.setState({value: this.props.signal()}))
+  }
+  render() {
+    return <CounterPresentation value={this.state.value}
+      onIncrement={() => over(r.inc, this.props.signal)} />
+  }
+}
+
+const App = () => <div>
+  <Counter signal={s.data(0)} />
+  <Counter signal={s.data(1)} />
+</div>
+
+s.root(() =>
+  render(<App />, document.getElementById('root'))
+)
