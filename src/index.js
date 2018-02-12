d2c1e1dc4e06866cd41f4a7d0e38f0b85857b376
Alex Bepple <alex@bepple.de>
2018-02-12 21:59:34 +0100

    . generalize state management

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as React from 'react'
 import {render} from 'react-dom'
 import s from 's-js'
 import * as r from 'ramda'
+import * as re from 'recompose'
 
 const signalLens = r.lens(s => s(), (val, s) => s(val))
 const over = r.over(signalLens)
@@ -11,23 +12,26 @@ const CounterPresentation = (props) => <div>
   <button onClick={props.onIncrement}>+</button>
 </div>
 
-class Counter extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {value: props.signal()}
-  }
-  componentDidMount() {
-    s(() => this.setState({value: this.props.signal()}))
-  }
-  render() {
-    return <CounterPresentation value={this.state.value}
-      onIncrement={() => over(r.inc, this.props.signal)} />
-  }
-}
+const connect = mapOwnProps => re.compose(
+  re.withProps(mapOwnProps),
+  re.lifecycle({
+    componentDidMount() {
+      s(() => this.setState(mapOwnProps(this.props)))
+    }
+  })
+)
 
+const Counter = connect(own => ({
+  value: own.signal(),
+  onIncrement: () => over(r.inc, own.signal)
+}))(CounterPresentation)
+
+const s1 = s.data(0)
+const s2 = s.data(1)
 const App = () => <div>
-  <Counter signal={s.data(0)} />
-  <Counter signal={s.data(1)} />
+  <Counter signal={s1} />
+  <Counter signal={s2} />
+  <Counter signal={s2} />
 </div>
 
 s.root(() =>
