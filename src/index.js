856a3c0ebb1aa2c60fe877809416c5e34e3f3cef
Alex Bepple <alex@bepple.de>
2018-02-12 22:03:05 +0100

    . extract module

--- src/index.js
+++ src/index.js
@@ -2,28 +2,16 @@ import * as React from 'react'
 import {render} from 'react-dom'
 import s from 's-js'
 import * as r from 'ramda'
-import * as re from 'recompose'
-
-const signalLens = r.lens(s => s(), (val, s) => s(val))
-const over = r.over(signalLens)
+import * as z from './z'
 
 const CounterPresentation = (props) => <div>
   <h2>Count: {props.value}</h2>
   <button onClick={props.onIncrement}>+</button>
 </div>
 
-const connect = mapOwnProps => re.compose(
-  re.withProps(mapOwnProps),
-  re.lifecycle({
-    componentDidMount() {
-      s(() => this.setState(mapOwnProps(this.props)))
-    }
-  })
-)
-
-const Counter = connect(own => ({
+const Counter = z.connect(own => ({
   value: own.signal(),
-  onIncrement: () => over(r.inc, own.signal)
+  onIncrement: () => z.over(r.inc, own.signal)
 }))(CounterPresentation)
 
 const s1 = s.data(0)
---
+++ src/z.js
@@ -0,0 +1,15 @@
+import s from 's-js'
+import * as r from 'ramda'
+import * as re from 'recompose'
+
+const signalLens = r.lens(s => s(), (val, s) => s(val))
+export const over = r.over(signalLens)
+
+export const connect = mapOwnProps => re.compose(
+  re.withProps(mapOwnProps),
+  re.lifecycle({
+    componentDidMount() {
+      s(() => this.setState(mapOwnProps(this.props)))
+    }
+  })
+)
