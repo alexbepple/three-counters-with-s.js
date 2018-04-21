4b7b8f889d3a895707dd0d590d987f14c450cea6
Alex Bepple <alex@bepple.de>
2018-04-21 23:40:55 +0200

    $ add headings

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import * as React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
 import s from 's-js'
-import {Counter} from './Counter'
+import { Counter } from './Counter'
 
 const s1 = s.data(0)
 const s2 = s.data(1)
-const App = () => <div>
-  <Counter signal={s1} />
-  <Counter signal={s2} />
-  <Counter signal={s2} />
-</div>
-
-s.root(() =>
-  render(<App />, document.getElementById('root'))
+const App = () => (
+  <div>
+    <h1>Independent counter</h1>
+    <Counter signal={s1} />
+    <h1>Linked counters</h1>
+    <Counter signal={s2} />
+    <Counter signal={s2} />
+  </div>
 )
+
+s.root(() => render(<App />, document.getElementById('root')))
